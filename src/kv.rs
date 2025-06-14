16fdda271320f3c00f0f0dfe4edcbbb4a0ce4450
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2025-06-14 14:54:07 -0400

    rust-dev: kv: update documentation

--- src/kv.rs
+++ src/kv.rs
@@ -2,6 +2,22 @@ use crate::command::{Command, CommandExecutionError};
 use crate::key::construct_key;
 use std::fs::{read_to_string, write};
 
+/// `KeyValueStore` implements a key-value store which uses the local
+/// filesystem for persistence. One important aspect of the key-value store
+/// is that it does not support deletion -- as it is not required for version
+/// control and could lead to repository corruption.
+///
+/// # Variants
+///
+/// - `Global`:
+///   Represents a global key-value store that is not partition-specific. This
+///   is generally used to store repository-wide data.
+///
+/// - `Partition(&'a String)`:
+///   Represents a partitioned key-value store with a reference to a `String`
+///   that specifies the name of the partition. Any data local to a partition
+///   is stored within its associated k-v store.
+/// ```
 pub enum KeyValueStore<'a> {
     Global,
     Partition(&'a String),
@@ -52,7 +68,7 @@ impl Command for KVCommand {
                 let hash_key = construct_key(key);
 
                 let Some(value) = kv_store.get(&hash_key) else {
-                    println!("Key {key} not found");
+                    println!("Key '{key}' not found");
                     return Ok(());
                 };
                 println!("{}", value);
@@ -68,6 +84,8 @@ impl Command for KVCommand {
                 kv_store.put(&hash_key, value);
                 Ok(())
             }
+            // Prints out the internal hash key used for a given key. This is useful
+            // for introspection to see how blaze stores data internally.
             KVCommand::Hash { key } => {
                 let hash_key = construct_key(key);
                 println!("{}", hash_key);
