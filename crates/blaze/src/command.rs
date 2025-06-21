e7315c10c6d93f4c5470bdb23e0d7eab7ce9b401
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2025-06-20 22:02:04 -0400

    blaze: remove unused CommandExecutionError struct

--- crates/blaze/src/command.rs
+++ crates/blaze/src/command.rs
@@ -1,18 +1,4 @@
 use anyhow::Result;
-use std::fmt;
-use std::fmt::Formatter;
-
-/// Error to indicate a failure during the execution of a command.
-#[derive(Debug, Clone)]
-pub struct CommandExecutionError {
-    pub(crate) message: String,
-}
-
-impl fmt::Display for CommandExecutionError {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
 
 /// Trait that represents a generic command that can be executed.
 pub trait Command {
